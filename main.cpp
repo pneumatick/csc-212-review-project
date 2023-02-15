15a818012323764be83e31cca4aa0c9090a80eab
jasonduquette <123509299+jasonduquette@users.noreply.github.com>
2023-02-15 14:40:18 -0500

    Update main.cpp
    Added functionality in the menu options in main.cpp. I also created a gradebook object for the sake of having something for the class functions to be called from. Can change it later if we want

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "gradebook.h"
 
 int main() {
     bool exit = false;
     std::string value;
+    Gradebook grades;
 
     while(!exit){
         std::cout<<"Input the number for the menu choice you want executed: "<<std::endl;
@@ -15,16 +17,42 @@ int main() {
         std::cin>>value;
 
         if(value == "1"){
+            //Figured this format for these variables is simple enough, option# then category (c) or name (n)
+            std::string opt1_c;
+            std::cout << "Input the category of the assignment you are looking for:" << std::endl;
+            std::cin >> opt1_c;
 
+            std::string opt1_n;
+            std::cout << "Input the name of the assignment:" << std::endl;
+            std::cin >> opt1_n;
+
+            grades.getAssignmentGrade(opt1_c, opt1_n);
         }
         else if(value == "2"){
+            std::string opt2_c;
+            std::cout << "Input the name of the category you want the total grade for:" std::endl;
+            std::cin >> opt2_c;
 
+            grades.getCategoryGrade(opt2_c);
         }
         else if (value == "3"){
-
+            grades.getTotalGrade();
         }
         else if(value == "4"){
+            std::string opt4_c;
+            std::string opt4_n;
+            std::string opt4_g;
 
+            std::cout << "Input the name of the category for the assignment you're adding:" << std::endl;
+            std::cin >> opt4_c;
+
+            std::cout << "Input the name of the assignment you're adding:" << std::endl;
+            std::cin >> opt4_n;
+
+            std::cout << "Input the grade of the assignment you're adding:" << std::endl;
+            std::cin >> opt4_g;
+
+            grades.addGrade(opt4_c, opt4_n, opt4_g);
         }
         else if(value == "5"){
             exit = true;
