c6164f3b274d7f8bb8bb791b97bce74c9d6b255a
Erik <proto@DESKTOP-O89RR9M>
2023-02-17 22:28:16 -0500

    Consolidated user input variables.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include "gradebook.h"
 
 int main(int argc, char*argv[]) {
-    bool exit = false;
+    bool exit = false;  // Menu exit condition
     std::string value;
     // Create a gradebook with data from a text file
     Gradebook grades(argv[1]);
+    std::string user_input_category;
+    std::string user_input_name;
+    double user_input_grade;
 
     while(!exit){
+        //  Main menu
         std::cout<<"Input the number for the menu choice you want executed: "<<std::endl;
         std::cout<<"1. Get the grade from a specific assignment."<<std::endl;
         std::cout<<"2. Get the grade for a specific group of assignments "<<std::endl;
@@ -17,43 +21,36 @@ int main(int argc, char*argv[]) {
 
         std::cin>>value;
 
+        // Sub menus
         if(value == "1"){   // Grade for single assignment
-            std::string opt1_c;
             std::cout << "Input the category of the assignment you are looking for:" << std::endl;
-            std::cin >> opt1_c;
+            std::cin >> user_input_category;
 
-            std::string opt1_n;
             std::cout << "Input the name of the assignment:" << std::endl;
-            std::cin >> opt1_n;
+            std::cin >> user_input_name;
 
-            std::cout << "Grade for assignment " << opt1_n << " in category " << opt1_c << ":\n"
-                                     << grades.get_assignment_grade(opt1_c, opt1_n) << "%\n\n";
+            std::cout << "Grade for assignment " << user_input_name << " in category " << user_input_category << ":\n"
+                                     << grades.get_assignment_grade(user_input_category, user_input_name) << "%\n\n";
         }
         else if(value == "2"){  // Grade for entire category
-            std::string opt2_c;
             std::cout << "Input the name of the category you want the total grade for:" << std::endl;
-            std::cin >> opt2_c;
+            std::cin >> user_input_category;
 
-            std::cout << "Grade for category " << opt2_c << ":\n" << grades.get_category_grade(opt2_c) << "%\n\n";
+            std::cout << "Grade for category " << user_input_category << ":\n" << grades.get_category_grade(user_input_category) << "%\n\n";
         }
         else if (value == "3"){ // Grade for overall course
             std::cout << "Overall course grade:\n" << grades.get_total_grade() << "\n\n";
         }
         else if(value == "4"){  // Add new grade to gradebook
-            std::string opt4_c;
-            std::string opt4_n;
-            std::string opt4_g;
-
             std::cout << "Input the name of the category for the assignment you're adding:" << std::endl;
-            std::cin >> opt4_c;
+            std::cin >> user_input_category;
 
             std::cout << "Input the name of the assignment you're adding:" << std::endl;
-            std::cin >> opt4_n;
+            std::cin >> user_input_name;
 
             std::cout << "Input the grade of the assignment you're adding:" << std::endl;
-            std::cin >> opt4_g;
 
-            grades.add_grade(opt4_c, opt4_n, std::stod(opt4_g));
+            grades.add_grade(user_input_category, user_input_name, user_input_grade);
         }
         else if(value == "5"){  // Exit program
             exit = true;
