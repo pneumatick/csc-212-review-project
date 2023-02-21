18ad0d40bb710f315926b00f4d4e5760dd47e9f7
pneumatick <jhwwefan@gmail.com>
2023-02-21 11:44:45 -0500

    Fixed bug in add_grade; more cleanup

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char*argv[]) {
     Gradebook grades;
     std::string user_input_category;
     std::string user_input_name;
-    double user_input_grade;
+    std::string user_input_grade;
     std::string filename = "grades.txt";
 
     // Ensure that the program is run with the correct number of args.
@@ -21,12 +21,13 @@ int main(int argc, char*argv[]) {
 	    grades = Gradebook(filename);
 	    break;
 	default:
-	    std::cout << "Unexpected number of args: Requires none (new gradebook) or one input file name (existing gradebook)" << std::endl;
+	    std::cout << "Unexpected number of args: Requires none (new gradebook)"
+		<< " or one input file name (existing gradebook)" << std::endl;
 	    return 1;
     }
 
     while (!exit) {
-        //  Main menu
+        // Main menu
         std::cout << "Input the number for the menu choice you want executed: " << std::endl;
         std::cout << "1. Get the grade from a specific assignment." << std::endl;
         std::cout << "2. Get the grade for a specific group of assignments " << std::endl;
@@ -36,43 +37,50 @@ int main(int argc, char*argv[]) {
 
         std::cin >> value;
 
-        // Sub menus
+        // Handle menu options
         if (value == "1") {   // Grade for single assignment
-            std::cout << "Input the category of the assignment you are looking for:" << std::endl;
+            std::cout << "\nInput the category of the assignment you are looking for: ";
             std::cin >> user_input_category;
 
-            std::cout << "Input the name of the assignment:" << std::endl;
+            std::cout << "\nInput the name of the assignment: ";
             std::cin >> user_input_name;
 
-            std::cout << "Points achieved for assignment \"" << user_input_name << "\" in category \"" << user_input_category << "\":\n"
-                                     << grades.get_assignment_grade(user_input_category, user_input_name) << "\n\n";
+            std::cout << "\nPoints achieved for assignment \"" 
+		<< user_input_name << "\" in category \"" << user_input_category << "\": " 
+		<< grades.get_assignment_grade(user_input_category, user_input_name) 
+		<< "\n\n";
         }
         else if (value == "2") {  // Grade for entire category
-            std::cout << "Input the name of the category you want the total grade for:" << std::endl;
+            std::cout << "\nInput the name of the category you want the total grade for: ";
             std::cin >> user_input_category;
 
-            std::cout << "Grade for category \"" << user_input_category << "\":\n" << grades.get_category_grade(user_input_category) << "%\n\n";
+            std::cout << "\nGrade for category \"" << user_input_category << "\": " 
+		<< grades.get_category_grade(user_input_category) << "%\n\n";
         }
         else if (value == "3") { // Grade for overall course
-            std::cout << "Overall course grade:\n" << grades.get_total_grade() << "\n\n";
+            std::cout << "\nOverall course grade: " << grades.get_total_grade() << "%\n\n";
         }
         else if (value == "4") {  // Add new grade to gradebook
-            std::cout << "Input the name of the category for the assignment you're adding:" << std::endl;
+            std::cout << "\nInput the name of the category for the assignment you're adding: ";
             std::cin >> user_input_category;
 
-            std::cout << "Input the name of the assignment you're adding:" << std::endl;
+            std::cout << "\nInput the name of the assignment you're adding: ";
             std::cin >> user_input_name;
 
-            std::cout << "Input the grade of the assignment you're adding:" << std::endl;
+            std::cout << "\nInput the grade of the assignment you're adding: ";
+	    std::cin >> user_input_grade;
 
-            grades.add_grade(user_input_category, user_input_name, user_input_grade);
+            grades.add_grade(user_input_category, user_input_name, std::stod(user_input_grade));
+
+	    std::cout << "\n" + user_input_category + " " + user_input_name 
+		+ " with a grade of " + user_input_grade + " added!\n" << std::endl;
         }
         else if (value == "5") {  // Exit program, update grades file
             exit = true;
             grades.close(filename);
         }
         else {   // Invalid user input
-           std::cout<<"Invalid input try again" <<std::endl;
+           std::cout << "Invalid input: Try again" << std::endl;
         }
 
     }
